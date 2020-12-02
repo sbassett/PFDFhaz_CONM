4844735555a0ccd7ddbdc66a1d93f667bee67d27
Steve Bassett <sbassett@tnc.org>
2020-12-02 00:47:30 -0700

    Outlined debris flow hazard code.

---
+++ pseudocode.r
@@ -0,0 +1,37 @@
+# Pseudo Code Outline for Post-fire Debris Flow Hazard
+
+#will translate this into a R Notebook 
+
+
+
+
+#Define Analysis Extent
+
+#Generate HPUs
+
+  #Load DEM Data
+  
+  #Load Sinks
+  
+  #Generate Stream Networks
+  
+  #Generate Candidate HPUs
+  
+  #Select Final HPUs
+
+#Generate Input Parameter Grids
+
+  #Burn Severity (simple linear canopy cover model)
+
+  #Precipitation Intensity
+
+  #...
+( fefw(wdede()))
+
+#Zonal Stats of Input Parameter Grids
+
+#Run the Debris Flow Hazard Models
+
+#Export Data
+
+#Visualize Data
